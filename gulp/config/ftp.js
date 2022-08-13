e387b122b1e2b248bf193cef797978c52dec2d63
Alekseii <1914.ac@gmail.com>
2022-08-13 21:16:50 +0300

    changed file of gitignore

--- gulp/config/ftp.js
+++
@@ -1,9 +0,0 @@
-export let configFTP = {
-  /* host: "files.000webhost.com", // Адрес FTP сервера
-  user: "dev-geis", // Имя пользователя
-  password: "Ucy2h8FfAGLFdtC", // Пароль */
-  host: "ftp.dev-geis.pp.ua",
-  user: "u110391844.dev",
-  password: "u2=TghatjgE~",
-  parallel: 5, // Кол-во одновременных подключений
-};
